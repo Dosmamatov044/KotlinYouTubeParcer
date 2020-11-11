c1f0866228902c1f91a9bb661c0e39b088a73693
Dosmamatov044 <dosmamatov24@gmail.com>
2020-11-11 21:58:22 +0600

    1

---
+++ app/src/main/java/com/example/kotlinyoutubeparcer/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.kotlinyoutubeparcer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    var key:String  = "AIzaSyCO__x6rsVAwStDJukt7yHGVP7mDHY7u38"
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
